f1442acee278fdeb2dcf730d037fa953505b1e80
Isaac0906 <isaac@111percent.net>
2021-04-21 12:07:45 +0900

    first commit

---
+++ Runtime/Submodule.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Submodule : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("This is submodule1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
